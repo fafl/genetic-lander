b6eee4cf5b29594d7c8c548b8fd45757d92fe67a
Fabian Flechtmann <fabian.flechtmann@sap.com>
2016-04-27 13:47:53 +0200

    Object orientation

--- script.js
+++ script.js
@@ -10,84 +10,91 @@ var level1data = [
     "4000 2000 0 0 9750 0 0"
 ]
 
-var parseLevel = function(levelData) {
-    var fields = levelData[0].split(" ");
-    var level = {
-        timestep    : 0,
-        width       : parseInt(fields[0]),
-        height      : parseInt(fields[1]),
-        g           : parseFloat(fields[2]),
-        max_thrust  : parseInt(fields[7]),
-        min_angle   : parseInt(fields[8]),
-        max_angle   : parseInt(fields[9])
-    };
-    var numberOfPoints  = parseInt(levelData[1])
-    level.points = [];
-    for (var i = 0; i < numberOfPoints; i++) {
-        var points = levelData[i+2].split(" ");
-        level.points.push([
-            parseInt(points[0]),
-            parseInt(points[1])
-        ]);
-    }
-    fields = levelData[i+2].split(" ")
-    level.lander = {
-        points   : [], // List of past points for drawing
-        x       : parseInt(fields[0]),
-        y       : parseInt(fields[1]),
-        xspeed  : parseInt(fields[2]),
-        yspeed  : parseInt(fields[3]),
-        fuel    : parseInt(fields[4]),
-        angle   : parseInt(fields[5]),
-        power   : parseInt(fields[6])
-    };
-    level.tick = function() {
+var Lander = {
+    read: function(fields) {
+        this.points = [], // List of past points for drawing
+        this.x      = parseInt(fields[0]),
+        this.y      = parseInt(fields[1]),
+        this.xspeed = parseInt(fields[2]),
+        this.yspeed = parseInt(fields[3]),
+        this.fuel   = parseInt(fields[4]),
+        this.angle  = parseInt(fields[5]),
+        this.power  = parseInt(fields[6])
+    },
+    tick: function(g) {
         this.timestep += 1;
-        this.lander.points.push([this.lander.x, this.lander.y]);
+        this.points.push([this.x, this.y]);
 
         // Advance the lander one timestep
         // From https://forum.codingame.com/t/mars-lander-puzzle-discussion/32/129        
-        this.lander.fuel -= this.lander.power;
-        var arcAngle = -this.lander.angle * Math.PI / 180;
-        var xacc = Math.sin(arcAngle) * this.lander.power;
-        var yacc = Math.cos(arcAngle) * this.lander.power - this.g;
-        this.lander.xspeed += xacc;
-        this.lander.yspeed += yacc;
-        this.lander.x += this.lander.xspeed - (xacc * 0.5);
-        this.lander.y += this.lander.yspeed - (yacc * 0.5);
+        this.fuel -= this.power;
+        var arcAngle = -this.angle * Math.PI / 180;
+        var xacc = Math.sin(arcAngle) * this.power;
+        var yacc = Math.cos(arcAngle) * this.power - g;
+        this.xspeed += xacc;
+        this.yspeed += yacc;
+        this.x += this.xspeed - (xacc * 0.5);
+        this.y += this.yspeed - (yacc * 0.5);
     }
-    return level;
 }
 
-var draw = function(level) {
+var Level = {
+    read: function(levelData) {
+        var fields = levelData[0].split(" ");
+        this.timestep   = 0,
+        this.width      = parseInt(fields[0]),
+        this.height     = parseInt(fields[1]),
+        this.g          = parseFloat(fields[2]),
+        this.max_thrust = parseInt(fields[7]),
+        this.min_angle  = parseInt(fields[8]),
+        this.max_angle  = parseInt(fields[9])
+        var numberOfPoints  = parseInt(levelData[1])
+        this.points = [];
+        for (var i = 0; i < numberOfPoints; i++) {
+            var points = levelData[i+2].split(" ");
+            this.points.push([
+                parseInt(points[0]),
+                parseInt(points[1])
+            ]);
+        }
+        fields = levelData[i+2].split(" ")
+        this.lander = Object.create(Lander);
+        this.lander.read(fields);
+    },
+    draw: function() {
 
-    // Create svg element
-    var svgContainer = d3.select("body").append("svg")
-        .attr("width", level.width * 0.2)
-        .attr("height", level.height * 0.2)
-        .attr("viewBox", "0 " + level.height + " " + level.width + " 0")
-        .attr("viewBox", "0 0 " + level.width + " " + level.height)
+        // Create svg element
+        var svgContainer = d3.select("body").append("svg")
+            .attr("width", this.width * 0.2)
+            .attr("height", this.height * 0.2)
+            .attr("viewBox", "0 " + this.height + " " + this.width + " 0")
+            .attr("viewBox", "0 0 " + this.width + " " + this.height)
 
-    // Draw terrain
-    var polylineString = toPolylineString(level.points, level);
-    var terrain = svgContainer.append("polyline")
-        .attr("points", polylineString)
-        .style("stroke", "black")
-        .style("fill", "none")
+        // Draw terrain
+        var polylineString = this.toPolylineString(this.points);
+        var terrain = svgContainer.append("polyline")
+            .attr("points", polylineString)
+            .style("stroke", "black")
+            .style("fill", "none")
 
-    // Draw lander path
-    polylineString = toPolylineString(level.lander.points, level);
-    var path = svgContainer.append("polyline")
-        .attr("points", polylineString)
-        .style("stroke", "black")
-        .style("fill", "none")    
-}
+        // Draw lander path
+        polylineString = this.toPolylineString(level.lander.points);
+        var path = svgContainer.append("polyline")
+            .attr("points", polylineString)
+            .style("stroke", "black")
+            .style("fill", "none")    
+    },
+    toPolylineString: function(a) {
 
-var toPolylineString = function(a, level) {
-
-    // Converts an array of pairs to a polyline string
-    // Rotates y-axis
-    return a.map(function(x) {
-        return x[0] + "," + (level.height-x[1])
-    }).join(" ")
+        // Converts an array of pairs to a polyline string
+        // Rotates y-axis
+        var level = this;
+        var coords = a.map(function(x) {
+            return x[0] + "," + (level.height-x[1])
+        })
+        return coords.join(" ")
+    },
+    tick: function() {
+        this.lander.tick(this.g);
+    }
 }
