848bdca191b8a82bb3fa993d4ab1f36ccf365850
Fabian Flechtmann <fabian.flechtmann@sap.com>
2016-04-27 16:14:42 +0200

    Now we have random landers

--- script.js
+++ script.js
@@ -6,20 +6,40 @@ var level1data = [
     "7000 3000 3.711 1.0 1.0 1 0 4 -90 90",
     "9",
     "0 2500", "100 100", "500 50", "1000 300", "2000 300",
-    "2500 0", "6500 50", "6899 100", "6999 2500",
+    "2100 10", "6500 250", "6899 200", "6999 2500",
     "4000 2000 0 0 9750 0 0"
 ]
 
+var Foo = {
+    createRandomLanderMoves: function(count, initialAngle) {
+        var result = []
+        var angle = initialAngle || 0;
+        for (var i = 0; i < count; i++) {
+            angle += Foo.getRandomInt(-15, 15)
+            angle = Math.min(angle, 90)
+            angle = Math.max(angle, -90)
+            power = Foo.getRandomInt(0, 4)
+            result.push([angle, power]);
+        }
+        return result;
+    },
+    getRandomInt: function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
+
 var Lander = {
-    read: function(fields) {
-        this.points = [], // List of past points for drawing
-        this.x      = parseInt(fields[0]),
-        this.y      = parseInt(fields[1]),
-        this.xspeed = parseInt(fields[2]),
-        this.yspeed = parseInt(fields[3]),
-        this.fuel   = parseInt(fields[4]),
-        this.angle  = parseInt(fields[5]),
+    init: function(fields, color) {
+        this.points = [] // List of past points for drawing
+        this.color  = color || "black"
+        this.x      = parseInt(fields[0])
+        this.y      = parseInt(fields[1])
+        this.xspeed = parseInt(fields[2])
+        this.yspeed = parseInt(fields[3])
+        this.fuel   = parseInt(fields[4])
+        this.angle  = parseInt(fields[5])
         this.power  = parseInt(fields[6])
+        return this;
     },
     tick: function(g) {
         this.timestep += 1;
@@ -39,14 +59,15 @@ var Lander = {
 }
 
 var Level = {
-    read: function(levelData) {
+    init: function(levelData) {
+        this.timestep   = 0
+        this.landers    = []
         var fields = levelData[0].split(" ");
-        this.timestep   = 0,
-        this.width      = parseInt(fields[0]),
-        this.height     = parseInt(fields[1]),
-        this.g          = parseFloat(fields[2]),
-        this.max_thrust = parseInt(fields[7]),
-        this.min_angle  = parseInt(fields[8]),
+        this.width      = parseInt(fields[0])
+        this.height     = parseInt(fields[1])
+        this.g          = parseFloat(fields[2])
+        this.max_thrust = parseInt(fields[7])
+        this.min_angle  = parseInt(fields[8])
         this.max_angle  = parseInt(fields[9])
         var numberOfPoints  = parseInt(levelData[1])
         this.points = [];
@@ -57,9 +78,8 @@ var Level = {
                 parseInt(points[1])
             ]);
         }
-        fields = levelData[i+2].split(" ")
-        this.lander = Object.create(Lander);
-        this.lander.read(fields);
+        this.defaultLanderFields = levelData[i+2].split(" ")
+        return this;
     },
     draw: function() {
 
@@ -75,14 +95,18 @@ var Level = {
         var terrain = svgContainer.append("polyline")
             .attr("points", polylineString)
             .style("stroke", "black")
+            .style("stroke-width", "10")
             .style("fill", "none")
 
         // Draw lander path
-        polylineString = this.toPolylineString(level.lander.points);
-        var path = svgContainer.append("polyline")
-            .attr("points", polylineString)
-            .style("stroke", "black")
-            .style("fill", "none")    
+        for (var i = 0; i < this.landers.length; i++) {
+            polylineString = this.toPolylineString(level.landers[i].points);
+            svgContainer.append("polyline")
+                .attr("points", polylineString)
+                .style("stroke", level.landers[i].color)
+                .style("stroke-width", "10")
+                .style("fill", "none")
+        }
     },
     toPolylineString: function(a) {
 
@@ -95,6 +119,8 @@ var Level = {
         return coords.join(" ")
     },
     tick: function() {
-        this.lander.tick(this.g);
+        for (var i = 0; i < this.landers.length; i++) {
+            this.landers[i].tick(this.g);
+        }
     }
 }
